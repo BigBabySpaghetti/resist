acaf71327a62f1d7a523014376b7de16a345a819
ksnapko-imricor <kyzr.snapko@imricor.com>
2025-06-30 15:08:14 -0500

    feat: added basic stuff from tutorial

--- resist/src/game/main.py
+++ resist/src/game/main.py
@@ -1,17 +1,30 @@
+from math import pi, sin, cos
+
 from direct.showbase.ShowBase import ShowBase
+from direct.task import Task
 
 
 class MyApp(ShowBase):
+
     def __init__(self):
-        super().__init__()
+        ShowBase.__init__(self)
 
-        self.set_background_color(0, 0, 0)
+        self.scene = self.loader.loadModel("models/environment")
 
-        # Load a model
-        self.model = self.loader.loadModel("models/environment")
-        self.model.reparentTo(self.render)
-        self.model.setScale(0.25, 0.25, 0.25)
-        self.model.setPos(-8, 42, 0)
+        self.scene.reparentTo(self.render)
+
+        self.scene.setScale(0.25, 0.25, 0.25)
+        self.scene.setPos(0, 0, 0)
+
+        self.taskMgr.add(self.spin_camera_task, "SpinCameraTask")
+
+
+    def spin_camera_task(self, task):
+        angle_degrees = task.time * 6.0
+        angle_radians = angle_degrees * (pi / 180.0)
+        self.camera.setPos(20 * sin(angle_radians), -20 * cos(angle_radians), 3)
+        self.camera.setHpr(angle_degrees, 0, 0)
+        return Task.cont
 
 
 app = MyApp()
